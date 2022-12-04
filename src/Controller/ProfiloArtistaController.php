8a782d164ad2fe452c2cd25edb70429818d2722f
simone.dolce <simonedolce26@gmail.com>
2022-12-04 18:06:32 +0100

    Primo Commit_2

---
+++ src/Controller/ProfiloArtistaController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfiloArtistaController extends AbstractController
+{
+    #[Route('/profilo/rapper', name: 'visualizza_profilo_artista')]
+    public function index(): Response
+    {
+        return $this->render('profilo_rapper/index.html.twig', [
+            'controller_name' => 'ProfiloArtistaController',
+        ]);
+    }
+
+    #[Route('/profilo/modifica', name: 'modifica_profilo_artista')]
+    public function modifica(): Response
+    {
+        return $this->render('profilo_rapper/modifica-profilo.html.twig', [
+            'controller_name' => 'ProfiloArtistaController',
+        ]);
+    }
+}
